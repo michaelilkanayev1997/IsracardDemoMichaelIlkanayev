dd6f08fe161d1a95d55e4df2db4d96b1bee2ea77
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-08-21 00:09:55 +0300

    refactor(home): extract BookCard into a separate memoized component for reuse and performance

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,7 @@
 import { FC, useMemo, useState } from "react";
 import { FlashList } from "@shopify/flash-list";
-import { View, Text, Image, StyleSheet, Platform } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
 
 import { useGetBooksQuery } from "@/slices/booksApiSlice";
 import Loader from "@/components/Loader";
@@ -11,6 +10,7 @@ import EmptyState from "@/components/EmptyState";
 import SearchBar from "@/components/SearchBar";
 import Header from "@/components/Header";
 import colors from "@/constants/colors";
+import BookCard from "@/components/BookCard";
 
 const BooksScreen: FC = () => {
   const { data, isLoading, isError, refetch } = useGetBooksQuery();
@@ -56,24 +56,7 @@ const BooksScreen: FC = () => {
               icon="book-outline"
             />
           }
-          renderItem={({ item }) => (
-            <View style={styles.card}>
-              <Image source={{ uri: item.cover }} style={styles.image} />
-              <View style={styles.info}>
-                <Text style={styles.title} numberOfLines={2}>
-                  {item.title}
-                </Text>
-                <View style={styles.dateRow}>
-                  <Ionicons
-                    name="calendar-outline"
-                    size={16}
-                    color={colors.TEXT_SECONDARY}
-                  />
-                  <Text style={styles.release}>{item.releaseDate}</Text>
-                </View>
-              </View>
-            </View>
-          )}
+          renderItem={({ item }) => <BookCard book={item} />}
         />
       )}
     </SafeAreaView>
@@ -87,45 +70,6 @@ const styles = StyleSheet.create({
   },
   listContent: {
     padding: 16,
-  },
-  card: {
-    flexDirection: "row",
-    backgroundColor: colors.SURFACE,
-    borderRadius: 12,
-    padding: 12,
-    marginBottom: 14,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOpacity: 0.08,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    elevation: 3,
-  },
-  image: {
-    width: 70,
-    height: 100,
-    borderRadius: 8,
-    resizeMode: "cover",
-  },
-  info: {
-    flex: 1,
-    marginLeft: 14,
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: Platform.OS === "ios" ? "600" : "bold",
-    color: colors.TEXT_PRIMARY,
-    marginBottom: 6,
-  },
-  dateRow: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: 2,
-  },
-  release: {
-    fontSize: 14,
-    color: colors.TEXT_SECONDARY,
-    marginLeft: 6,
   },
 });
 
