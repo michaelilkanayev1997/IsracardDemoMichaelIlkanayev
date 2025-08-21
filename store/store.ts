d5fc8e8a3924bb230b47525538d04207c668afb7
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-08-21 16:23:43 +0300

    feat(redux): add favorites reducer to store

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,13 @@
 import { apiSlice } from "@/slices/apiSlice";
 import { configureStore } from "@reduxjs/toolkit";
 
+import favoritesReducer from "@/slices/favoritesSlice";
+
 // Create and configure the Redux store
 const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer, // RTK Query
+    favorites: favoritesReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
