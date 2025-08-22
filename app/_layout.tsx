61da0971646b4821451631036d7670eac45a87a8
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-08-22 11:22:03 +0300

    chore: wrap app with PersistGate in layout
    - Added <PersistGate> to persist Redux state across app restarts
    - Display <Loader /> component while rehydrating persisted store

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,27 +3,31 @@ import { Provider } from "react-redux";
 import { TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-import store from "@/store/store";
+import store, { persistor } from "@/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import Loader from "@/components/Loader";
 
 const RootLayout = () => {
   return (
     <Provider store={store}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="book/[id]"
-          options={{
-            headerShown: true,
-            animation: "slide_from_right",
-            title: "Book Details",
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => router.back()}>
-                <Ionicons name="arrow-back" size={24} color="black" />
-              </TouchableOpacity>
-            ),
-          }}
-        />
-      </Stack>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="book/[id]"
+            options={{
+              headerShown: true,
+              animation: "slide_from_right",
+              title: "Book Details",
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => router.back()}>
+                  <Ionicons name="arrow-back" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            }}
+          />
+        </Stack>
+      </PersistGate>
     </Provider>
   );
 };
